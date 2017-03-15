6c8d07f8b3762bac307e92c9d729cdfc06c27619
geekx <geekx63@gmail.com>
2017-03-15 23:54:58 +0800

    细节修改

--- javascript/boutique.js
+++ javascript/boutique.js
@@ -1,4 +1,13 @@
 $(function(){
+    var st = getQueryString('st');
+    if(st){
+        $('.boutique-item-' + st).addClass('active');        
+        $('.boutique-section-container').hide();
+        $('.tab-'+st).fadeIn();
+    }else{
+        $('.boutique-item-1').addClass('active');
+    }
+
     $('.boutique-item').click(function(e){
         var tab = $(this).attr('target-data');
         
@@ -8,4 +17,13 @@ $(function(){
         $('.boutique-section-container').hide();
         $('.'+tab).fadeIn();
     })
+
+    function getQueryString(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return unescape(r[2]);
+        }
+        return null;
+    }
 })
